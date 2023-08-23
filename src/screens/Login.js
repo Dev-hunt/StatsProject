2e7893066a3a40d2acf1eba21969cfa2f47443f9
Devansh <devanshdhamani@gmail.com>
2023-08-24 00:41:15 +0530

    update in login signup

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,46 @@
-import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View , Image, TouchableOpacity , ScrollView} from 'react-native'
+import React, { useState } from 'react'
 import Field from '../components/Field';
 
 const Login = ({navigation},{props}) => {
+  const [user, setUser]=useState({
+    Email_Address:"",
+    Password:"",
+
+  });
+  const [errormsg, setErrormsg] = useState(null);
+  const Sendtobackend=()=>{
+    console.log(user);
+    if (user.Email_Address==''|| user.Password==''){
+      setErrormsg('All Fields are Required');
+      return;
+    }
+    else{
+      fetch('http://10.0.2.2.:5000/signin',{
+        method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(user)
+      })
+      .then(res => res.json()).then(
+        data =>{
+          if(data.error){
+            setErrormsg(data.error);
+          }
+          else{
+            alert('Login SuccessFully');
+            navigation.navigate('Dashboard');
+          }
+        }
+        
+
+      )
+    }
+
+  }
   return (
-    <View>
+    <View style={{width:"100%",height:"100%"}}>
     <View style={{}}>
       <Image source={require('../asstes/antiSocial.png')} style={[styles.image,]}/>
       </View>
@@ -12,8 +48,15 @@ const Login = ({navigation},{props}) => {
       <Text style={[styles.welcometxt]}>Welcome To DigiDost</Text>
       {/* <Text style={[styles.digidost]}>DigiDost</Text> */}
       <View style={{marginTop:20}}>
-      <Field placeholder= "Email / Username" keyboardType={"email-address"}/>
-      <Field placeholder="Password" secureTextEntry={true}/>
+      {
+        errormsg ? <Text style={[styles.errormessage]}>{errormsg}</Text>:null
+      }
+      <Field placeholder= "Email / Username" keyboardType={"email-address"}
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Email_Address:text})}/>
+      <Field placeholder="Password" secureTextEntry={true}
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Password:text})}/>
       </View>
       </View>
       <View style={[styles.fp1,]}>
@@ -21,7 +64,8 @@ const Login = ({navigation},{props}) => {
       </View>
       <View >
       <TouchableOpacity 
-        onPress={() => navigation.navigate('Dashboard')}
+        // onPress={() => navigation.navigate('Dashboard')}
+        onPressIn={()=> Sendtobackend()}
         >
             <Text style={styles.but}>  Login  </Text>
       </TouchableOpacity>
@@ -32,8 +76,7 @@ const Login = ({navigation},{props}) => {
           <Text style={[styles.signup,]}>Signup</Text>
         </TouchableOpacity>
       </View>
-
-    </View>
+      </View>
   )
 }
 
@@ -42,8 +85,8 @@ export default Login;
 const styles = StyleSheet.create({
   image:
   {
-    height:240,
-    width:240,
+    height:140,
+    width:140,
     // alignSelf:"flex-end",
     marginLeft:160,
     // marginBottom:730,
@@ -99,6 +142,134 @@ const styles = StyleSheet.create({
   {
     fontWeight:'bold',
     color:'#110D59',
+  },
+  errormessage:
+  {
+    color:'red',
+    fontSize: 15,
+    textAlign:"center",
+    padding: 9,
+    borderRadius: 15,
   }
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StyleSheet, Text, View , Image, TouchableOpacity} from 'react-native'
+// import React from 'react'
+// import Field from '../components/Field';
+
+// const Login = ({navigation},{props}) => {
+//   return (
+//     <View>
+//     <View style={{}}>
+//       <Image source={require('../asstes/antiSocial.png')} style={[styles.image,]}/>
+//       </View>
+//       <View>
+//       <Text style={[styles.welcometxt]}>Welcome To DigiDost</Text>
+//       {/* <Text style={[styles.digidost]}>DigiDost</Text> */}
+//       <View style={{marginTop:20}}>
+//       <Field placeholder= "Email / Username" keyboardType={"email-address"}/>
+//       <Field placeholder="Password" secureTextEntry={true}/>
+//       </View>
+//       </View>
+//       <View style={[styles.fp1,]}>
+//         <Text style={[styles.fp,]}>Forgot Password ?</Text>
+//       </View>
+//       <View >
+//       <TouchableOpacity 
+//         onPress={() => navigation.navigate('Dashboard')}
+//         >
+//             <Text style={styles.but}>  Login  </Text>
+//       </TouchableOpacity>
+//       </View>
+//       <View style={{display:'flex', flexDirection:'row',justifyContent:"center"}}>
+//         <Text>Don't have an account ?</Text>
+//         <TouchableOpacity onPress={()=> navigation.navigate("Signup")}>
+//           <Text style={[styles.signup,]}>Signup</Text>
+//         </TouchableOpacity>
+//       </View>
+
+//     </View>
+//   )
+// }
+
+// export default Login;
+
+// const styles = StyleSheet.create({
+//   image:
+//   {
+//     height:240,
+//     width:240,
+//     // alignSelf:"flex-end",
+//     marginLeft:160,
+//     // marginBottom:730,
+//     borderRadius:70,
+//     // position:'absolute'
+//   },  
+
+//   welcometxt:
+//   {
+//     fontSize:40,
+//     fontWeight:"bold",
+//     color:'#110D59',
+//     alignSelf:"flex-start",
+//     paddingHorizontal:"10%"
+//   },
+//   digidost:
+//   {
+//     fontSize:40,
+//     fontWeight:"bold",
+//     color:'#110D59',
+//     paddingHorizontal:70,
+//     alignSelf:"flex-start",
+//   },
+//   fp1:
+//   {
+//     alignItems:"flex-end",
+//     width:"89%",
+//     marginBottom:"22%",
+//     // marginTop:20,
+
+
+//   },
+//   fp:
+//   {
+//     fontWeight:"bold",
+//     color:'#110D59',
+//   },
+//   but:
+//   {
+//       color:"white",
+//       textAlign:"center",
+//       marginVertical:10,
+//       backgroundColor:"#181277",
+//       padding:9,
+//       paddingLeft:44,
+//       paddingRight:44,
+//       alignSelf:"center",
+//       elevation: 8,
+//       borderRadius: 32,
+//       fontSize:34,
+//   },
+//   signup:
+//   {
+//     fontWeight:'bold',
+//     color:'#110D59',
+//   }
+
+// })
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,19 +1,97 @@
-import { StyleSheet, Text, View , Image, TouchableOpacity,} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View , Image, TouchableOpacity,TextInput} from 'react-native'
+import React, {useState} from 'react'
 import Field from '../components/Field';
 
 const Signup =({navigation},{props}) => {
+
+  const [user, setUser] = useState({
+    Name:"",
+    Email_Address:"",
+    Password:"",
+    Confirm_Password:"",
+  });
+
+  const [errormsg, setErrormsg] = useState(null);
+
+  const Sendtobackend = () =>{
+    //console.log(user);
+    if(user.Name == "" ||
+    user.Confirm_Password == ''||
+    user.Email_Address == ''||
+    user.Password == ''){
+      setErrormsg('All fields are required');
+      return ;
+    }
+    else{
+      if(user.Password != user.Confirm_Password){
+        setErrormsg('Password and Confirm Password must be same');
+        return;
+      }
+      else{
+        fetch('http://10.0.2.2.:5000/signup',{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(user)
+        })
+          .then(res => res.json()).then(
+            data =>{
+              if(data.error){
+                setErrormsg(data.error);
+              }
+              else{
+                alert('account created successfully');
+                navigation.navigate('Login');
+              }
+            }
+          )
+      }
+    }
+  }
   return (
+
     <View>
       <Image source={require('../asstes/antiSocial.png')} style={[styles.image,]}/>
       <Text style={[styles.welcometxt]}>Create a new account </Text>
-      <Field placeholder= "Name " />
-      <Field placeholder= "Email Address " keyboardType={"email-address"}/>
-      <Field placeholder="Password" secureTextEntry={true}/>
-      <Field placeholder="Confirm Password" secureTextEntry={true}/>
+      {
+        errormsg ? <Text style={[styles.errormessage]}>{errormsg}</Text>:null
+      }
+      <Field 
+      placeholder= "Name " 
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Name:text})
+        // console.log(text)
+      }
+      />
+       {/* <TextInput 
+        placeholder= "Name " 
+        onChangeText={(Text)=>
+        {
+          console.log(Text)
+        }}
+       
+      /> */}
+      <Field placeholder= "Email Address " keyboardType={"email-address"}
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Email_Address:text})}/>
+      <Field placeholder="Password" secureTextEntry={true}
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Password:text})}/>
+      <Field placeholder="Confirm Password" secureTextEntry={true}
+      onPressIn={()=>setErrormsg(null)}
+      onChangeText={(text) => setUser({ ...user, Confirm_Password:text})}/>
 
       <TouchableOpacity 
-        onPress={() => navigation.navigate("Dashboard")}
+        onPress={() => {
+          Sendtobackend();
+        }
+          
+// Anshuuu yha pr code likhnaaaa
+  
+
+          // navigation.navigate("Dashboard")
+        }
         >
             <Text style={styles.but}>  Register </Text>
       </TouchableOpacity>
@@ -23,7 +101,7 @@ const Signup =({navigation},{props}) => {
           <Text style={[styles.signup,]}>Login</Text>
         </TouchableOpacity>
       </View>
-
+      
     </View>
   )
 }
@@ -33,8 +111,8 @@ export default Signup;
 const styles = StyleSheet.create({
   image:
   {
-    height:240,
-    width:240,
+    height:140,
+    width:140,
     // alignSelf:"flex-end",
     marginLeft:160,
     borderRadius:80,
@@ -88,5 +166,118 @@ const styles = StyleSheet.create({
     fontWeight:'bold',
     color:'#110D59',
   },
-
+  errormessage:
+  {
+    color:'red',
+    fontSize: 15,
+    textAlign:"center",
+    padding: 9,
+    borderRadius: 15,
+  }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StyleSheet, Text, View , Image, TouchableOpacity,} from 'react-native'
+// import React from 'react'
+// import Field from '../components/Field';
+
+// const Signup =({navigation},{props}) => {
+//   return (
+//     <View>
+//       <Image source={require('../asstes/antiSocial.png')} style={[styles.image,]}/>
+//       <Text style={[styles.welcometxt]}>Create a new account </Text>
+//       <Field placeholder= "Name " />
+//       <Field placeholder= "Email Address " keyboardType={"email-address"}/>
+//       <Field placeholder="Password" secureTextEntry={true}/>
+//       <Field placeholder="Confirm Password" secureTextEntry={true}/>
+
+//       <TouchableOpacity 
+//         onPress={() => navigation.navigate("Dashboard")}
+//         >
+//             <Text style={styles.but}>  Register </Text>
+//       </TouchableOpacity>
+//       <View style={{display:'flex', flexDirection:'row',justifyContent:"center"}}>
+//         <Text> Already have an account ?</Text>
+//         <TouchableOpacity onPress={()=>navigation.navigate("Login")}>
+//           <Text style={[styles.signup,]}>Login</Text>
+//         </TouchableOpacity>
+//       </View>
+
+//     </View>
+//   )
+// }
+
+// export default Signup;
+
+// const styles = StyleSheet.create({
+//   image:
+//   {
+//     height:240,
+//     width:240,
+//     // alignSelf:"flex-end",
+//     marginLeft:160,
+//     borderRadius:80,
+//   },  
+
+//   welcometxt:
+//   {
+//     fontSize:29,
+//     fontWeight:"bold",
+//     color:'#110D59',
+//     alignSelf:"flex-start",
+//     paddingHorizontal:"10%"
+//   },
+// //   digidost:
+// //   {
+// //     fontSize:40,
+// //     fontWeight:"bold",
+// //     color:'#110D59',
+// //     paddingHorizontal:70,
+// //     alignSelf:"flex-start",
+// //   },
+//   fp1:
+//   {
+//     alignItems:"flex-end",
+//     width:"85%",
+//     marginBottom:"20%",
+
+
+//   },
+//   fp:
+//   {
+//     fontWeight:"bold",
+//     color:'#110D59',
+//   },
+//   but:
+//   {
+//       color:"white",
+//       textAlign:"center",
+//       marginVertical:10,
+//       backgroundColor:"#181277",
+//       padding:9,
+//       paddingLeft:44,
+//       paddingRight:44,
+//       alignSelf:"center",
+//       elevation: 8,
+//       borderRadius: 32,
+//       fontSize:34,
+//   },
+//   signup:
+//   {
+//     fontWeight:'bold',
+//     color:'#110D59',
+//   },
+
+// })
